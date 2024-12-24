f74f2a4729f360d17d26a919d80766a49ee34483
Lindauro Neto <netolindauro@gmail.com>
2024-12-24 15:27:46 -0300

    ajuste do problema da persistência dos dados

--- src/main/java/br/com/nald/LiterAlura/model/Autor.java
+++ src/main/java/br/com/nald/LiterAlura/model/Autor.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import br.com.nald.LiterAlura.dto.DadosAutor;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,9 @@ public class Autor {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@Column(unique = true)
 	private String nome;
+	
 	private Integer dataNascimento;
 	private Integer dataFalecimento;
 	
