b58b09782cc66818df10ab1a99576c434545c377
Lindauro Neto <netolindauro@gmail.com>
2024-12-25 17:12:49 -0300

    ajustes finalizados nas classes principais de Service

--- src/main/java/br/com/nald/LiterAlura/service/LivroService.java
+++ src/main/java/br/com/nald/LiterAlura/service/LivroService.java
@@ -1,15 +1,75 @@
 package br.com.nald.LiterAlura.service;
 
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.nald.LiterAlura.dto.DadosAutor;
 import br.com.nald.LiterAlura.dto.DadosLivro;
 import br.com.nald.LiterAlura.dto.RequesicaoDto;
+import br.com.nald.LiterAlura.exceptions.MaisDeUmAutorException;
 import br.com.nald.LiterAlura.model.Autor;
+import br.com.nald.LiterAlura.model.Livro;
+import br.com.nald.LiterAlura.repository.LivroRepository;
 
 @Service
 public class LivroService {
 	private ConversaoDados conversor = new ConversaoDados();
+	private ConsumoAPI consumo = new ConsumoAPI();
+	
+	@Autowired
+	private LivroRepository repositorio;
+	
+	
+	
+	public void listandoAutoresPelaData(Integer dataAno) {
+		Optional<List<Autor>> autoresVivos = repositorio.autoresAindaVivos(dataAno);
+		
+		if (autoresVivos.isPresent()) {
+			System.out.println("Segue lista de autores vivos em " + dataAno);
+			autoresVivos.get().forEach(System.out::println);
+		} else {
+			System.out.println("Não há autores vivos no ano de: " + dataAno);
+		}
+		
+	}
+	
+	public void listandoAutoresRegistrados() {
+		List<Autor> autores = repositorio.findAll().stream()
+				.map(l -> l.getAutor())
+				.collect(Collectors.toList());
+		autores.forEach(System.out::println);
+		
+	}
+	
+	public void listandoLivrosRegistrados() {
+		List<Livro> livros = repositorio.findAll();
+		livros.forEach(System.out::println);
+	}
+	
+	public void buscandoERegistrandoDeLivro(String url, String nomeLivro) {
+		String json = criacaoConexao(url, nomeLivro);
+		
+		DadosLivro dadosLivro = obtencaoDadosLivro(json);
+		Livro livro = new Livro(dadosLivro);
+		Autor autor = obtencaoDadosAutor(dadosLivro);
+		
+		Optional<Autor> autorExiste = repositorio.autoresJaExistentes(autor.getNome());
+		if (autorExiste.isEmpty()) {
+			livro.setAutor(autor);
+			autor.getLivros().add(livro);
+		} else {
+			throw new MaisDeUmAutorException("Nessa aplicação (até o momento), só se pode 1 autor por livro, escolha um livro escrito por um outro autor.");
+		}
+		
+		repositorio.save(livro);
+		
+		System.out.println(livro.toString());
+	}
 	
 	public DadosLivro obtencaoDadosLivro(String json) {
 		RequesicaoDto requesicao = conversor.obterDados(json, RequesicaoDto.class);
@@ -19,5 +79,11 @@ public class LivroService {
 	public Autor obtencaoDadosAutor(DadosLivro dadosLivro) {
 		DadosAutor dadosAutor = dadosLivro.autor().get(0);
 		return new Autor(dadosAutor);
+	}
+	
+	@SuppressWarnings("deprecation")
+	private String criacaoConexao(String url, String nomeLivro) {
+		String nome = URLEncoder.encode(nomeLivro); 
+		return consumo.obterDados(url + "?search=" + nome);
 	}
 }
--- src/main/java/br/com/nald/LiterAlura/service/PrincipalService.java
+++ src/main/java/br/com/nald/LiterAlura/service/PrincipalService.java
@@ -1,31 +1,18 @@
 package br.com.nald.LiterAlura.service;
 
-import java.net.URLEncoder;
-import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
-
-import br.com.nald.LiterAlura.dto.DadosLivro;
-import br.com.nald.LiterAlura.exceptions.MaisDeUmAutorException;
-import br.com.nald.LiterAlura.model.Autor;
-import br.com.nald.LiterAlura.model.Livro;
-import br.com.nald.LiterAlura.repository.LivroRepository;
 
 @Service
 public class PrincipalService {
 	
 	private final String urlApi = "https://gutendex.com/books/";
 	private Scanner scanner = new Scanner(System.in);
-	private ConsumoAPI consumo = new ConsumoAPI();
 	
-	private LivroRepository repositorio;
 	private LivroService servicoLivro;
 	
-	public PrincipalService(LivroRepository repositorio, LivroService servicoLivro) {
-		this.repositorio = repositorio;
+	public PrincipalService( LivroService servicoLivro) {
 		this.servicoLivro = servicoLivro;
 	}
 	
@@ -42,22 +29,22 @@ public class PrincipalService {
 		System.out.println(menuIdiomas);
 		var opcao = scanner.nextLine().toLowerCase();
 		switch (opcao) {
-		case "es":
-			
-			break;
-		case "en":
-			
-			break;
-		case "fr":
-			
-			break;
-		case "pt":
-			
-			break;
-
-		default:
-			System.out.println("Opção inválida");
-			break;
+			case "es":
+				
+				break;
+			case "en":
+				
+				break;
+			case "fr":
+				
+				break;
+			case "pt":
+				
+				break;
+	
+			default:
+				System.out.println("Opção inválida");
+				break;
 		}
 	}
 	
@@ -66,59 +53,29 @@ public class PrincipalService {
 		var dataAno = scanner.nextInt();
 		scanner.nextLine();
 		
-		Optional<List<Autor>> autoresVivos = repositorio.autoresAindaVivos(dataAno);
-		
-		if (autoresVivos.isPresent()) {
-			System.out.println("Segue lista de autores vivos em " + dataAno);
-			autoresVivos.get().forEach(System.out::println);
-		} else {
-			System.out.println("Não há autores vivos no ano de: " + dataAno);
-		}
+		servicoLivro.listandoAutoresPelaData(dataAno);
 		
 	}
 	
 	public void listagemAutores() {
-		List<Autor> autores = repositorio.findAll().stream()
-				.map(l -> l.getAutor())
-				.collect(Collectors.toList());
-		autores.forEach(System.out::println);
+		servicoLivro.listandoAutoresRegistrados();
 		
 	}
 	
 	public void listagemLivros() {
 		System.out.println("Livros registrados: ");
-		List<Livro> livros = repositorio.findAll();
-		livros.forEach(System.out::println);
+		servicoLivro.listandoLivrosRegistrados();
 		
 	}
 	
 	public void buscaLivro() {
 		System.out.println("Digite o nome do livro: ");
 		var nomeLivro = scanner.nextLine();
-		String json = criacaoConexao(nomeLivro);
 		
-		DadosLivro dadosLivro = servicoLivro.obtencaoDadosLivro(json);
-		Livro livro = new Livro(dadosLivro);
-		Autor autor = servicoLivro.obtencaoDadosAutor(dadosLivro);
+		servicoLivro.buscandoERegistrandoDeLivro(urlApi, nomeLivro);
 		
-		Optional<Autor> autorExiste = repositorio.autoresJaExistentes(autor.getNome());
-		if (autorExiste.isEmpty()) {
-			livro.setAutor(autor);
-			autor.getLivros().add(livro);
-		} else {
-			throw new MaisDeUmAutorException("Nessa aplicação (até o momento), só se pode 1 autor por livro, escolha um livro escrito por um outro autor.");
-		}
-		
-		repositorio.save(livro);
-		
-		System.out.println(livro.toString());
-//		System.out.println(autor.toString());
 	}
 	
-	@SuppressWarnings("deprecation")
-	private String criacaoConexao(String nomeLivro) {
-		String nome = URLEncoder.encode(nomeLivro); 
-		return consumo.obterDados(urlApi + "?search=" + nome);
-	}
+	
 
 }
